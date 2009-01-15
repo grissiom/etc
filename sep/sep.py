grissiom/etc

commit 2a5c6dae961029bd1c6c70688bfccae519dbf789

make copy the default operation.

sep/sep.py
@@ -97,14 +97,14 @@ try:
 		KEEP = False
 	else:
 		KEEP = True
-	#parse -c
-	if 'c' in argv:
-		argv.remove('c')
-		OPER_FILE_MOD = 'copy'
-		operate_file = copy2
-	else:
+	#parse -m
+	if 'm' in argv:
+		argv.remove('m')
 		OPER_FILE_MOD = 'move'
 		operate_file = move
+	else:
+		OPER_FILE_MOD = 'copy'
+		operate_file = copy
 	#parse -v
 	if 'v' in argv:
 		argv.remove('v')
@@ -145,7 +145,7 @@ Any thing behand "-t" will treat as the file type you want to move.'
 destdir must goes before any options'
 -r:do the reverse work, i.e, extract the files from destdir to cwd.'
 -n:no recursion'
--c:copy the file instead of moing the file'
+-m:copy the file instead of copying the file'
 -k:keep the empty derctories.'
 -v:make the output being verbose'
 	''')

commit 845a810ac05feb315ac6d6f48781f3756ba563f8

fix a typo
fix a typo introduced by commit 2a5c6dae961029bd1c6c70688bfccae519dbf789

sep/sep.py
@@ -95,7 +95,7 @@ try:
 		operate_file = move
 	else:
 		OPER_FILE_MOD = 'copy'
-		operate_file = copy
+		operate_file = copy2
 	#parse -v
 	if 'v' in argv:
 		argv.remove('v')