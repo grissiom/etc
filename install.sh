78b0d75bc3c266f564f0d40056557ae9e91d1c29
Grissiom <chaos.proton@gmail.com>
2009-05-18 04:07:18 +0800

    install: remove devtodo

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ ln -fsT ${PWD}/bash_config/bash_profile ~/.bash_profile
 ln -fsT ${PWD}/bash_config/profile.d ~/.profile.d
 ln -fsT ${PWD}/bash_config/bashrc ~/.bashrc
 ln -fsT ${PWD}/bash_config/bashrc.d ~/.bashrc.d
-# Konsole seems not be a login shell. So link devtodo to bashrc.d to make use
-# of it. Git only track profile.d/devtodo.sh.
-ln -fsT ${PWD}/bash_config/profile.d/devtodo.sh ${PWD}/bash_config/bashrc.d/devtodo.sh
+## Konsole seems not be a login shell. So link devtodo to bashrc.d to make use
+## of it. Git only track profile.d/devtodo.sh.
+#ln -fsT ${PWD}/bash_config/profile.d/devtodo.sh ${PWD}/bash_config/bashrc.d/devtodo.sh
 
 # Vim staff:
 ln -fsT ${PWD}/vim/vimrc ~/.vimrc
