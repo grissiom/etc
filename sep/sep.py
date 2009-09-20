aac5bc7018d6797297c7fc37db32c9bd19df8af7
Grissiom <chaos.proton@gmail.com>
2009-09-20 23:35:52 +0800

    rewrite sep.py
    Finally got sep.py smaller while do more ;)

--- sep/sep.py
+++ sep/sep.py
@@ -1,183 +1,178 @@
-#!/usr/bin/python
+#!/bin/env python
 
 import re
-from os import listdir, chdir, path, getcwd, makedirs, rmdir, remove
-from os.path import isdir, isfile, abspath
-from os.path import join as join_path
+import os
+from os import listdir, makedirs, rmdir, remove
+from os.path import isdir, isfile, islink
+joinp, splitp = os.path.join, os.path.split
 from shutil import move, copy2
-from sys import argv, exit
+import sys
 
-class option_er(Exception):
-	def __init__(self, value):
-		self.value = value
+def exit_err(no, msg, stream=sys.stderr):
+        sys.stdout.flush()
+        stream.write('\n' + get_color('red') +
+                         msg + get_color('default') + '\n')
+        sys.exit(no)
 
-	def __str__(self):
-		return repr(self.value)
-
-class SystemExit2(SystemExit):
-	def __init__(self, message = 0, strerror = None):
-		self.strerror = get_color('red') + strerror + get_color('default')
-		SystemExit.__init__(self, message)
-
-def dummy(*args):
-	pass
-
-# I hope this piece of code could be sweept out permanently after switched to Python3
-def printf(*args):
-	print ' '.join(map(str, args))
+def printf(arg, lv=0, stream=sys.stdout):
+        if lv <= PRINT_LV:
+                stream.write(arg)
 
 def get_color(cl):
-	return colours.get(cl, '')
+        return colours.get(cl, '')
 
-def movefiles(ftypes, destdir):
-	test = re.compile("." + "(" + "|".join(ftypes) + ")" + "$", re.IGNORECASE)
-	files = filter(test.search, listdir('.'))
-	files = filter(isfile, files)
-	if len(files) == 0:
-		print_v('no file to be operate in ' + getcwd())
-		return
-	if isdir(destdir) is False:
-		try:
-			makedirs(destdir)
-		except OSError, oserr:
-			raise SystemExit2(3, 'Error when creating %s: %s' %
-					(oserr.filename, oserr.strerror))
-	for i in files:
-		try:
-			print(get_color('yellow') + OPER_FILE_MOD +' '+ i + ' to ' +
-                              destdir)
-			operate_file(i, join_path(destdir, i))
-			print(get_color('green') + " ... Done\n" +
-                              get_color('default'))
-		except IOError, ioerr:
-			remove(join_path(destdir, i))
-			raise SystemExit2(3, 'Error when %s %s: %s\nRemoved file:%s' %
-					(OPER_FILE_MOD, i, ioerr.strerror, i))
-		except KeyboardInterrupt, keyinter:
-			remove(join_path(destdir, i))
-			print("Remove " + join_path(destdir, i))
-			raise SystemExit2(1, 'User aborted.')
+# adapted from /usr/lib64/python2.6/os.py
+def walk_dir(top, topdown=True, followlinks=False, ml=0, rl=0):
+        '''walk like os.walk
 
-def main(ftypes, wdir, destdir):
-	try:
-		print_v('working in ' + wdir)
-		chdir(wdir)
-		if RECURSION:
-			dirs = filter(isdir, listdir('.'))
-			for i in dirs:
-				main(ftypes, join_path(wdir, i), join_path(destdir, i))
-		chdir(wdir)
-		movefiles(ftypes, destdir)
-		if KEEP and len(listdir(wdir)) == 0:
-			rmdir(wdir)
-			print_v('clean ' + wdir)
-	except KeyboardInterrupt:
-		raise SystemExit2(1, 'User aborted.')
+        ml is the max recursion level. Recursion check is disabled if ml == 0.
+        rl is the current recursion level. You don't need to set this variable
+            at normal cases.'''
+        if ml != 0 and rl >= ml:
+                return
+        try:
+                names = listdir(top)
+        except:
+                pass
+
+        dirs, nondirs = [], []
+        for name in names:
+                if isdir(joinp(top, name)):
+                        dirs.append(name)
+                else:
+                        nondirs.append(name)
+
+        if topdown:
+                yield top, dirs, nondirs
+        for name in dirs:
+                path = joinp(top, name)
+                if followlinks or not islink(path):
+                        for x in walk_dir(path, topdown, followlinks, rl + 1):
+                                yield x
+        if not topdown:
+                yield top, dirs, nondirs
+
+def movefiles(file_name, destdir):
+        if isdir(destdir) is False:
+                try:
+                        makedirs(destdir)
+                except OSError, oserr:
+                        exit_err(3, 'Error when creating %s: %s' %
+                                        (oserr.filename, oserr.strerror))
+        try:
+                printf(get_color('yellow') + OPER_FILE_MOD
+                      + ' ' + file_name + ' to '
+                      + destdir + os.sep + ' ... ')
+                # work around: shutil.move will fail if the dest file exits...
+                if OPERATE_FILE == move:
+                        if isfile(joinp(destdir, splitp(file_name)[1])):
+                                remove(joinp(destdir, splitp(file_name)[1]))
+                OPERATE_FILE(file_name, destdir)
+                printf(get_color('green') + "Done\n" + get_color('default'))
+        except IOError, ioerr:
+                if isfile(joinp(destdir, splitp(file_name)[1])):
+                        remove(joinp(destdir, splitp(file_name)[1]))
+                exit_err(3, 'Error when %s %s: %s' %
+                                (OPER_FILE_MOD, file_name, ioerr.strerror))
+        except KeyboardInterrupt:
+                if isfile(joinp(destdir, splitp(file_name)[1])):
+                        remove(joinp(destdir, splitp(file_name)[1]))
+                printf("Remove " + joinp(destdir, file_name) + '\n')
+                exit_err(1, 'User aborted.')
+
+def main(destdir):
+        try:
+                for root_dir, dirs, files in walk_dir('.', topdown=False, ml=RECURSION_LEVEL):
+                        files = [ i for i in files if FILE_FILTER.search(i)]
+                        if len(files) == 0:
+                                printf('no file to be operate in ' + root_dir + '\n', 1)
+                        for i in files:
+                                movefiles(
+                                   os.path.normpath(joinp(root_dir, i)),
+                                   os.path.normpath(joinp(destdir, root_dir)))
+                        if (not KEEP) and len(listdir(root_dir)) == 0 \
+                           and root_dir != '.':
+                                rmdir(root_dir)
+                                printf('clean ' + root_dir + '\n')
+        except KeyboardInterrupt:
+                exit_err(1, 'User aborted.')
 
 # the main body
-try:
-	cwd = getcwd()
 #----------- Begin the option parsing -----------#
-	#parse -t
-	try:
-		ftypes = argv[argv.index('-t') + 1:]
-	except:
-		raise option_er('-t option missing')
-	del argv[argv.index('-t'):]
-	# parse destdir
-	try:
-		destdir = argv[1]
-	except:
-		raise option_er('destdir missing')
-	del argv[1]
-	del argv[0]
-	if destdir in listdir('.'):
-		print("you have already have %s. Anything have the same file name will be overwrite." % destdir)
-	main_destdir = abspath(destdir)
-	main_wd = cwd
+argv = sys.argv[1:]
+opt_hlp, opt_err, tmp = [], [], []
+for i in argv:
+        if i[0] == '-':
+                tmp.extend([k for k in i if k != '-'])
+        else:
+                tmp.append(i)
+argv = tmp
 
-	argv = list(''.join(argv))
-	# parse -k
-	if 'k' in argv:
-		argv.remove('k')
-		KEEP = True
-	else:
-		KEEP = False
-	#parse -m
-	if 'm' in argv:
-		argv.remove('m')
-		OPER_FILE_MOD = 'move'
-		operate_file = move
-	else:
-		OPER_FILE_MOD = 'copy'
-		operate_file = copy2
-	#parse -v
-	if 'v' in argv:
-		argv.remove('v')
-		print_v=printf
-	else:
-		print_v=dummy
-	#paise -n
-	if 'n' in argv:
-		argv.remove('n')
-		RECURSION = False
-	else:
-		RECURSION = True
-	#paise -c
-	if 'c' in argv:
-		argv.remove('c')
-		colours = {
-                        'none'       :    "",
-                        'default'    :    "\033[0m",
-                        'bold'       :    "\033[1m",
-                        'underline'  :    "\033[4m",
-                        'blink'      :    "\033[5m",
-                        'reverse'    :    "\033[7m",
-                        'concealed'  :    "\033[8m",
+try:
+        ftypes = argv[-1]
+        FILE_FILTER = re.compile(ftypes, re.IGNORECASE)
+        del argv[-1]
+except:
+        opt_err.append('<pattern> missing')
 
-                        'black'      :    "\033[30m",
-                        'red'        :    "\033[31m",
-                        'green'      :    "\033[32m",
-                        'yellow'     :    "\033[33m",
-                        'blue'       :    "\033[34m",
-                        'magenta'    :    "\033[35m",
-                        'cyan'       :    "\033[36m",
-                        'white'      :    "\033[37m",
+opt_hlp.append('-n:recursion level(default is 100)\n')
+try:
+        RECURSION_LEVEL = int(argv[argv.index('-n') + 1])
+        del argv[argv.index('-n') + 1]
+        del argv[argv.index('-n')]
+except:
+        RECURSION_LEVEL = 100
 
-                        'on_black'   :    "\033[40m",
-                        'on_red'     :    "\033[41m",
-                        'on_green'   :    "\033[42m",
-                        'on_yellow'  :    "\033[43m",
-                        'on_blue'    :    "\033[44m",
-                        'on_magenta' :    "\033[45m",
-                        'on_cyan'    :    "\033[46m",
-                        'on_white'   :    "\033[47m",
+opt_hlp.append('-k:keep the empty directories.\n')
+KEEP = False
+if 'k' in argv:
+        argv.remove('k')
+        KEEP = True
 
-                        'beep'       :    "\007" }
-	else:
-		colours = {}
+opt_hlp.append('-m:move the file instead of copying the file\n')
+OPER_FILE_MOD = 'copy'
+OPERATE_FILE = copy2
+if 'm' in argv:
+        argv.remove('m')
+        OPER_FILE_MOD = 'move'
+        OPERATE_FILE = move
+
+opt_hlp.append('-v:make the output being verbose\n')
+PRINT_LV = 0
+if 'v' in argv:
+        argv.remove('v')
+        PRINT_LV = 1
+
+opt_hlp.append('-c:colorize output\n')
+colours = {}
+if 'c' in argv:
+        argv.remove('c')
+        colours = {'default':"\033[0m",
+                   'red'    :"\033[31m",
+                   'green'  :"\033[32m",
+                   'yellow' :"\033[33m",
+                   'blue'   :"\033[34m",
+                   }
+
+try:
+        destdir = argv[0]
+        del argv[0]
+except:
+        opt_err.append('destdir missing')
+
+if opt_err != []:
+        printf('Usage:sep.py <destdir> [Options] <pattern>\n\
+\n\
+Find files according to pattern and copy/move them to destdir. destdir must\n\
+goes before any options. pattern is python\'s regular expression. Only the\n\
+last pattern would take effect.\n\
+\n\
+Options:\n' + '\t' + '\t'.join(opt_hlp)
++ get_color('red') + ', '.join(opt_err) + get_color('default') + '\n')
+        sys.exit(2)
 #----------- End the option parsing -----------#
 
-	print_v('ftypes: ' + ftypes)
-	print_v('destdir: ' + destdir)
-	main(ftypes, main_wd, main_destdir)
-	exit(0)
-except SystemExit2, sysexit2:
-	print(sysexit2.strerror)
-except option_er, case:
-	print(
-'''Usage:sep <destdir> [Options] <-t file_type>
+printf('ftypes: ' + ftypes + '\n', 1)
+printf('destdir: ' + destdir + '\n', 1)
 
-Any thing behand "-t" will treat as the file type you want to move. destdir
-must goes before any options
-
-Options:
--n:no recursion
--m:copy the file instead of copying the file
--k:keep the empty derctories.
--c:colorize output
--v:make the output being verbose
-''')
-	print(case)
-	exit(2)
-
+main(destdir)
